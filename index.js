1fd0862fc8438966f498b40590b70923e71acc02
alixonoff <alixonakbaraliyev76@gmail.com>
2024-08-27 14:34:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const asos = document.getElementById('asosiy')
+ asos.style.display = "flex";
+ asos.style.justifyContent = "center"; 
+ asos.style.alignItems = "center";  
+//  asos.style.gap = "5px";
+ asos.style.textAlign = 'center'
+ asos.style.height = '500px'
+ asos.style.width = '700px'
+
+const ong = document.getElementById('ong')
+// ong.style.margin="90px"
+ong.style.background='white'
+ong.style.height='500px'
+ong.style.display = "flex";
+ong.style.flexDirection = "column";
+ong.style.justifyContent = "center";
+ong.style.alignItems = "center";  
+
+
+
+const sing = document.getElementById("sing")
+sing.style.display = "flex";
+sing.style.flexDirection = "column";
+sing.style.color="red"
+sing.style.width="350px"
+sing.style.gap="15px"
+sing.style.justifyContent = "center";
+sing.style.alignItems = "center";  
+
+const ind = document.querySelector(".in");
+ind.style.height="35px"
+ind.style.width="80%"
+
+const ins = document.querySelector(".is");
+ins.style.height = "35px";
+ins.style.width="80%"
+
+const inj = document.querySelector(".ij");
+inj.style.height = "40px";
+inj.style.background="#05976A"
+inj.style.width="80%"
+
+const chap = document.getElementById('chap')
+chap.style.background='#05976A'
+chap.style.color='white'
+chap.style.padding='60px'
+chap.style.height='380px'
+chap.style.display = "flex";
+chap.style.flexDirection = "column";
+chap.style.justifyContent = "center";
+chap.style.alignItems = "center";  
+
+
+const p = document.getElementById('p')
+p.style.width = "200px"
+
+const bu = document.getElementById('but')
+bu.style.padding='20px'
+bu.style.borderRadius = "21px"
+bu.style.opacity = "70%"
+bu.style.background = "#2af1b6";
+bu.style.border= '1px solid black'
